9e944b4f563c2cae0b30ede06b1fa6e7defd3327
kitso sejake <sejakekitso@gmail.com>
2025-10-30 10:34:56 +0200

    made the menu dropdown and scrollabe

--- src/components/config/SideMenu.ts
+++ src/components/config/SideMenu.ts
@@ -22,14 +22,36 @@ export const sidebarMenu = [
   },
   {
     label: "Work",
+     defaultOpen: true,
     items: [
       { label: "My Projects", icon: FolderOpen, path: "/projects" },
       { label: "Wallet", icon: Wallet, path: "/wallet" },
       { label: "Reviews", icon: Star, path: "/reviews" },
     ],
   },
+    {
+    label: "Badges",
+     defaultOpen: true,
+    items: [
+      { label: "Badges ", icon: FolderOpen, path: "/badges" },
+      { label: "Awards", icon: Wallet, path: "/" },
+      { label: "skills", icon: Star, path: "/" },
+      { label: "skills", icon: Star, path: "/" },
+    ],
+  },
+   {
+    label: "Resources",
+     defaultOpen: true,
+    items: [
+      { label: "Freelancing Toolkit", icon: FolderOpen, path: "/Resource" },
+      { label: "Pricing Guides", icon: Wallet, path: "/" },
+      { label: "Pitching strategies", icon: Star, path: "/" },
+      { label: "skills", icon: Star, path: "/" },
+    ],
+  },
   {
     label: "Account",
+     defaultOpen: true,
     items: [
       { label: "Profile", icon: User, path: "/profile" },
       { label: "Settings", icon: Settings, path: "/settings" },
